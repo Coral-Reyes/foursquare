3bd7f71e6a9f6536f611f1bb253f2b72721b0d24
Coral Reyes <koralflor11@gmail.com>
2017-02-02 13:25:46 +0000

    Js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // JavaScript File
+$(document).ready(function(){
+    $("#submit").click(function(){
+        var searchVal1= $("#address").val();
+        var searchVal2= $("#food-type").val();
+        var part1= "https://api.foursquare.com/v2/venues/search?client_id=JIPRBMQEVEQULL1MBCTX0LOMI4I51E1MJLFMIXH5GOYNFOP1&client_secret=VFV05CVYSJ3AIV2LGII52YZS5EMQKDJ5T3031FIK1C2WROTE&v=20130815&near="
+        var url = part1 + searchVal1 + "&query=" +searchVal2;
+        
+        $.getJSON(url,function(response){
+            var name= response.venues.name;
+            var location= response.venues.location.formattedAddress;
+            var number= response.venues.contact.formattedPhone;
+            
+            $("body").append(name);
+            $("body").append(location);
+            $("body").append(number);
+        });
+    };
+});
+//"&query="
